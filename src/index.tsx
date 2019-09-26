e37fe6d90c39c15d6ecdad9f09b9ae65a216b06e
Bernard <bero4net@gmail.com>
2019-09-26 22:13:43 +0200

    add typescript types and forward props to input component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {DetailedHTMLProps, InputHTMLAttributes} from 'react';
 
-const NumberInput: React.FunctionComponent = () => {
-  return (<input type="number" />);
+interface OwnProps {}
+
+type Props = OwnProps & Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'type'>;
+
+const NumberInput: React.FunctionComponent<Props> = ({...props}) => {
+  return (<input type="number" {...props}/>);
 };
 
 export default NumberInput;
