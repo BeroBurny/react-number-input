056c674e115e2a3531908cc7d837c9c96f132703
Bernard <bero4net@gmail.com>
2019-09-27 21:15:09 +0200

    implement basic cursor logic and value setter

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, {ChangeEvent, FunctionComponent, useEffect, useRef, useState} from 'react';
 import {Props} from "./types";
 
-const NumberInput: React.FunctionComponent<Props> = ({...props}) => {
-  return (<input type="number" {...props}/>);
+const NumberInput: FunctionComponent<Props> = ({value: propsValue , onBlur, onChange, ...props}) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [value, setValue] = useState(propsValue.toLocaleString());
+  const [selectionStart, setSelectionStart] = useState(0);
+  const [selectionEnd, setSelectionEnd] = useState(0);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { target } = event;
+
+    // save cursor position
+    setSelectionStart(target.selectionStart || 0);
+    setSelectionEnd(target.selectionEnd || 0);
+    setValue(target.value);
+  };
+
+  useEffect(
+    () => {
+      if (inputRef.current) {
+        inputRef.current.value = value;
+
+        // make it happen
+        inputRef.current.selectionStart = selectionStart;
+        inputRef.current.selectionEnd = selectionEnd;
+      }
+    },
+    [value]
+  );
+
+  return (<input
+    {...props}
+    onChange={handleChange}
+    type="text"
+    ref={inputRef}
+  />);
 };
 
 export default NumberInput;
--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,7 @@ type Callback = (value: number) => void;
 interface OwnProps {
   value: number,
   onChange?: Callback,
+  onBlur?: Callback,
 }
 
 export type Props = OwnProps & Omit<DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'type' | keyof OwnProps>;
