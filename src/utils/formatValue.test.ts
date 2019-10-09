de574b37164ac92a587638a250f39c92f5334ec6
Bernard <bero4net@gmail.com>
2019-10-09 23:33:24 +0200

    implement test for `formatValue` and `isCursorOnSeparator`

---
+++ src/utils/formatValue.test.ts
@@ -0,0 +1,19 @@
+import formatValue from "./formatValue";
+
+test('if value is a number', () => {
+  const result = formatValue(1000);
+
+  expect(result).toBe('1.000');
+});
+
+test('if value is a number and \'us\' separator', () => {
+  const result = formatValue(1000, 0, 'us');
+
+  expect(result).toBe('1,000');
+});
+
+test('if value is a with digits', () => {
+  const result = formatValue(1000, 3);
+
+  expect(result).toBe('1.000,000');
+});
---
+++ src/utils/isCursorOnSeparator.test.ts
@@ -0,0 +1,10 @@
+import isCursorOnSeparator from "./isCursorOnSeparator";
+
+test('on value 12.345.6789', () => {
+  const value = '12.345.6789';
+  expect(isCursorOnSeparator(value, 2)).toBeTruthy();
+  expect(isCursorOnSeparator(value, 3)).not.toBeTruthy();
+  expect(isCursorOnSeparator(value, 6)).toBeTruthy();
+  expect(isCursorOnSeparator(value, 8)).not.toBeTruthy();
+});
+
